f4c54f022aea65b0cafc16870a2540727df27c1a
Mawejje Nicholas <mawejjenikolas@gmail.com>
2022-11-17 19:02:56 +0300

    removed api word

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import authRouter from "./routes/auth.js";
 import userRouter from "./routes/user.js";
 import postRouter from "./routes/post.js";
 import catRouter from "./routes/category.js";
-import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
@@ -26,26 +25,26 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("backend has connected to the database"))
   .catch((error) => {
-    console.log(error);
+    console.log(`error is ${error}`);
   });
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  }, //image name will have to change
-});
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("file has been uploaded");
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, req.body.name);
+//   }, //image name will have to change
+// });
+// const upload = multer({ storage: storage });
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   res.status(200).json("file has been uploaded");
+// });
 
-app.use("/api/auth/", authRouter);
-app.use("/api/user/", userRouter);
-app.use("/api/posts/", postRouter);
-app.use("/api/categories", catRouter);
+app.use("/auth/", authRouter);
+app.use("/user/", userRouter);
+app.use("/posts/", postRouter);
+app.use("/categories/", catRouter);
 
 const PORT = process.env.PORT || 5000;
 
